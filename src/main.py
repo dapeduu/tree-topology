915a1638b5c610ae6b55ad2908321acf359d21f4
None <None>
2023-02-13 16:04:43 -0800

    adiciona remote controller

--- src/main.py
+++ src/main.py
@@ -9,11 +9,16 @@ and running sysctl -p. Check util/sysctl_addon.
 
 from mininet.cli import CLI
 from mininet.log import setLogLevel
-from mininet.node import OVSSwitch
+from mininet.node import OVSSwitch, RemoteController
 from mininet.topolib import TreeNet
 from mininet.examples.treeping64 import HostV4
 
 setLogLevel('info')
-network = TreeNet(depth=2, fanout=2, host=HostV4,
-                  switch=OVSSwitch, waitConnected=True)
+network = TreeNet(depth=3, fanout=2,
+                  host=HostV4,
+                  switch=OVSSwitch,
+                  waitConnected=True
+                  )
+network.addController('c1', controller=RemoteController,
+                      ip='127.0.0.1', port=6633)
 network.run(CLI, network)
